14fb194c5eb039f3b49d4dadf01043b27af95589
sayarkh <rahymgalievasaya@gamil.com>
2019-02-16 00:18:13 +0600

    Comment mistakes

---
+++ Week3/Task1/Task1/Farmanager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class FarManager
+    {
+        public int cursor;
+        public string path;
+        public int sz;
+        public bool ok;
+        DirectoryInfo directory = null;
+        FileSystemInfo current fs = null;
+
+        public FarManager()
+        {
+            cursor = 0;
+        }
+        public FarManager(string path)
+        {
+            this.path = path;
+            cursor  
+        }
+        public void Color()
+        {
+        }
+        public void Show()
+        {
+        }
+        public void Up()
+        {
+        }
+        public void Down()
+        {
+        }
+        public void Calcsz()
+        {
+        }
+        public void Start()
+        {
+        }
+
+        class Program
+        { 
+             static void Main(string[] args)
+        {
+        }
+    }
+}

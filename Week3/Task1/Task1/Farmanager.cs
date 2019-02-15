632d14e6dcaf9ea20c181a2843a5a59b254ed863
sayarkh <rahymgalievasaya@gamil.com>
2019-02-16 02:29:29 +0600

    Third lab

--- Week3/Task1/Task1/Farmanager.cs
+++ Week3/Task1/Task1/Farmanager.cs
@@ -14,40 +14,130 @@ namespace Task1
         public int sz;
         public bool ok;
         DirectoryInfo directory = null;
-        FileSystemInfo current fs = null;
+        FileSystemInfo currentFs = null;
 
         public FarManager()
         {
             cursor = 0;
         }
+
         public FarManager(string path)
         {
             this.path = path;
-            cursor  
+            cursor = 0;
+            directory = new DirectoryInfo(path);
+            sz = directory.GetFileSystemInfos().Length;
+            ok = true;
         }
-        public void Color()
+
+        public void Color(FileSystemInfo fs, int index)
         {
+            if (cursor == index)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                currentFs = fs;
+            }
+            else if (fs.GetType() == typeof(DirectoryInfo))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
         }
+
         public void Show()
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            for (int i = 0, k = 0; i < fs.Length; i++)
+            {
+                if (ok == false && fs[i].Name[0] == '.')
+                {
+                    continue;
+                }
+                Color(fs[i], k);
+                Console.WriteLine(fs[i].Name);
+                k++;
+            }
         }
         public void Up()
         {
+            cursor--;
+            if (cursor < 0)
+                cursor = sz - 1;
         }
         public void Down()
         {
-        }
-        public void Calcsz()
-        {
-        }
-        public void Start()
-        {
+            cursor++;
+            if (cursor == sz)
+                cursor = 0;
         }
 
-        class Program
-        { 
-             static void Main(string[] args)
+        public void CalcSz()
         {
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            sz = fs.Length;
+            if (ok == false)
+                for (int i = 0; i < fs.Length; i++)
+                    if (fs[i].Name[0] == '.')
+                        sz--;
+        }
+
+        public void Start()
+        {
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+            while (consoleKey.Key != ConsoleKey.Escape)
+            {
+                CalcSz();
+                Show();
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.UpArrow)
+                    Up();
+                if (consoleKey.Key == ConsoleKey.DownArrow)
+                    Down();
+                if (consoleKey.Key == ConsoleKey.RightArrow)
+                {
+                    ok = false;
+                    cursor = 0;
+                }
+                if (consoleKey.Key == ConsoleKey.LeftArrow)
+                {
+                    cursor = 0;
+                    ok = true;
+                }
+                if (consoleKey.Key == ConsoleKey.Enter)
+                {
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        path = currentFs.FullName;
+                    }
+                }
+                if (consoleKey.Key == ConsoleKey.Backspace)
+                {
+                    cursor = 0;
+                    path = directory.Parent.FullName;
+                }
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "/Users/askar/projects/PP2";
+            FarManager farManager = new FarManager(path);
+            farManager.Start();
         }
     }
 }
